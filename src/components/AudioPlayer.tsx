2069534e622841e9c0db27aba2e549bd9b0fa588
Daniel Hägglund <daniel.hagglund+github@gmail.com>
2018-02-24 19:32:45 +0100

    New player icons

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -1,4 +1,6 @@
 import { h } from 'hyperapp'
+import PauseIcon from './PauseIcon'
+import PlayIcon from './PlayIcon'
 
 export default ({ song, isPlaying, play, pause }) =>
   song ? (
@@ -8,15 +10,7 @@ export default ({ song, isPlaying, play, pause }) =>
         className="player__play-pause"
         onclick={() => (isPlaying ? pause() : play())}
       >
-        {isPlaying ? (
-          <svg viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1664 192v1408q0 26-19 45t-45 19h-512q-26 0-45-19t-19-45v-1408q0-26 19-45t45-19h512q26 0 45 19t19 45zm-896 0v1408q0 26-19 45t-45 19h-512q-26 0-45-19t-19-45v-1408q0-26 19-45t45-19h512q26 0 45 19t19 45z" />
-          </svg>
-        ) : (
-          <svg viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1576 927l-1328 738q-23 13-39.5 3t-16.5-36v-1472q0-26 16.5-36t39.5 3l1328 738q23 13 23 31t-23 31z" />
-          </svg>
-        )}
+        {isPlaying ? <PauseIcon /> : <PlayIcon />}
       </button>
       <div class="player__content">
         <div class="player__artist">{song.band}</div>
